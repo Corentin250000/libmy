d77ae349b4624ae7f0e9f1fe31037b31dab95754
corentin pasquier <corentin.pasquier@epitech.eu>
2024-12-09 15:33:20 +0100

    Update my_strcat Add a \0 at the end of concatenate string

--- my_strcat.c
+++ my_strcat.c
@@ -11,8 +11,10 @@ char *my_strcat(char *dest, char const *src)
 {
     int size_dest = my_strlen(dest);
     int size_src = my_strlen(src);
+    int j = 0;
 
-    for (int j = 0; j < size_src; j++)
+    for (; j < size_src; j++)
         dest[size_dest + j] = src[j];
+    dest[size_dest + j] = '\0';
     return dest;
 }
