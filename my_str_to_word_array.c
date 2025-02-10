4e989d32f3573d79bee9cce0ae9cbe317e06d77e
corentin pasquier <corentin.pasquier@epitech.eu>
2025-02-10 17:18:27 +0100

    Add unauthorized characters in str_to_word_array

--- my_str_to_word_array.c
+++ my_str_to_word_array.c
@@ -7,42 +7,42 @@
 
 #include "my.h"
 
-int is_authorized(char c, char const *authorized)
+int is_unauthorized(char c, char const *unauthorized)
 {
-    int present = 0;
+    int present = 1;
 
-    for (int i = 0; authorized[i] != '\0'; i++) {
-        if (c == authorized[i])
-            present = 1;
+    for (int i = 0; unauthorized[i] != '\0'; i++) {
+        if (c == unauthorized[i])
+            present = 0;
     }
     return present;
 }
 
-int size_not_word(char const *str, int index, char const *authorized)
+int size_not_word(char const *str, int index, char const *unauthorized)
 {
-    for (; !is_authorized(str[index], authorized)
+    for (; !is_unauthorized(str[index], unauthorized)
         && str[index] != '\0'; index++);
     return index;
 }
 
-int nb_word(char const *str, char const *authorized)
+int nb_word(char const *str, char const *unauthorized)
 {
-    int nbr = is_authorized(str[0], authorized);
+    int nbr = is_unauthorized(str[0], unauthorized);
 
     for (int i = 1; str[i] != '\0'; ++i) {
-        if (!is_authorized(str[i - 1], authorized) &&
-            is_authorized(str[i], authorized))
+        if (!is_unauthorized(str[i - 1], unauthorized) &&
+            is_unauthorized(str[i], unauthorized))
                 nbr++;
     }
     return nbr;
 }
 
-int size_word(char const *str, int index, char const *authorized)
+int size_word(char const *str, int index, char const *unauthorized)
 {
     int size = 0;
     int i = index;
 
-    for (; is_authorized(str[i], authorized); i++)
+    for (; is_unauthorized(str[i], unauthorized); i++)
         size++;
     return size;
 }
@@ -61,9 +61,9 @@ static char *fill_word(char const *str, int *index_str, int word_size)
     return word;
 }
 
-char **my_str_to_word_array(char const *str, char const *authorized)
+char **my_str_to_word_array(char const *str, char const *unauthorized)
 {
-    int word_number = nb_word(str, authorized);
+    int word_number = nb_word(str, unauthorized);
     int index_str = 0;
     int word_size = 0;
     char **result = malloc(sizeof(char *) * (word_number + 1));
@@ -71,8 +71,8 @@ char **my_str_to_word_array(char const *str, char const *authorized)
     if (!result)
         return NULL;
     for (int i = 0; i < word_number; i++) {
-        index_str = size_not_word(str, index_str, authorized);
-        word_size = size_word(str, index_str, authorized);
+        index_str = size_not_word(str, index_str, unauthorized);
+        word_size = size_word(str, index_str, unauthorized);
         result[i] = fill_word(str, &index_str, word_size);
         if (!result[i])
             return NULL;
